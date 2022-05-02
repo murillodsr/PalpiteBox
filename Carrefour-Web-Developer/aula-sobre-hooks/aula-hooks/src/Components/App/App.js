ba6199473da8ed05eea8a7e2945b54f9b9e7fcb6
Murillo <murillodsr@gmail.com>
2022-05-02 11:37:14 -0300

    first commit

--- Carrefour-Web-Developer/aula-sobre-hooks/aula-hooks/src/Components/App/App.js
+++ Carrefour-Web-Developer/aula-sobre-hooks/aula-hooks/src/Components/App/App.js
@@ -1,24 +1,14 @@
-import logo from './Components/App/logo.svg';
-import './Components/App/App.css';
+import './App.css';
+import Counter from '../Counter/Counter';
+import SmartCounter from '../Smartcounter/Smartcounter';
+import Ifoodcounter from '../Ifoodcounter/Ifoodcounter';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Hello World</h1>
+      <Ifoodcounter />
+    </>
   );
 }
 
--- Carrefour-Web-Developer/aula-sobre-hooks/aula-hooks/src/index.js
+++ Carrefour-Web-Developer/aula-sobre-hooks/aula-hooks/src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './Components/App';
+import App from './Components/App/App';
 
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
+
 
 
