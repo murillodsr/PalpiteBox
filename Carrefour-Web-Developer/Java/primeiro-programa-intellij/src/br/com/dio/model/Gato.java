e9ea1eff24bee2cc875aaa48ef216222e8d87f53
Murillo <murillodsr@gmail.com>
2022-05-02 11:41:55 -0300

    first commit

--- Carrefour-Web-Developer/Java/primeiro-programa-intellij/src/br/com/dio/model/Gato.java
+++ Carrefour-Web-Developer/Java/primeiro-programa-intellij/src/br/com/dio/model/Gato.java
@@ -58,7 +58,7 @@ public class Gato {
         return "Gato{" +
                 "nome='" + nome + '\'' +
                 ", cor='" + cor + '\'' +
-                ", idade=" + idade +
+                ", idade=" + idade  +
                 '}';
     }
 }
