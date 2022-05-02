a5c2af54581c8b7c222a3f8f8b6768f9c20b7d76
Murillo Souza <68777867+murillodsr@users.noreply.github.com>
2022-05-02 11:52:10 -0300

    Delete Carrefour-Web-Developer directory

--- Carrefour-Web-Developer/_Atividades/modulo3/scriptSet.js
+++
@@ -1,9 +0,0 @@
-const Array = [30, 30, 40, 5, 223, 2049, 3034, 5];
-
-function valoresUnicos(arr) {
-  const mySet = new Set(arr);
-
-  return [...mySet]; // Utilizado o argumento rest para que retorne um Array e não um Set.
-}
-
-console.log(valoresUnicos(Array));
